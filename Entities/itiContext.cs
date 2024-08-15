8ece28277fa1e35c2d74f6e8c11d96929b52ec8d
Nedal <nedalosama3@gmail.com>
2024-08-15 20:38:55 +0300

    Entity Framework

---
+++ Entities/itiContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass_1.Entities
+{
+    internal class itiContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=. ; Database=iti; Trusted_Connection=True; Encrypt=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course_Inst>()
+                .HasKey(ci => new { ci.Inst_ID, ci.Course_ID });
+            modelBuilder.Entity<Stud_Course>()
+             .HasKey(sc => new { sc.Stud_ID, sc.Course_ID });
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Stud_Course> StudCourses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Course_Inst> CourseInsts { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+    }
+}
